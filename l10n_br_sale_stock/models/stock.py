0d61d8977b84e695bee952ff8b271bf456781946
sadamo <daniel.sadamo@kmee.com.br>
2016-02-03 09:36:10 -0200

    [CLEAN] Removed commented code (_prepare_invoice method)

--- l10n_br_sale_stock/models/stock.py
+++ l10n_br_sale_stock/models/stock.py
@@ -24,47 +24,6 @@ from openerp import models
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    # TODO migrate to new API - Não existe mais este método
-    # def _prepare_invoice(self, cr, uid, picking, partner,
-    #                      inv_type, journal_id, context=None):
-    #     result = super(StockPicking, self)._prepare_invoice(
-    #         cr, uid, picking, partner, inv_type, journal_id, context)
-    #
-    #     fp_comment = []
-    #     fc_comment = []
-    #     fp_ids = []
-    #     fc_ids = []
-    #
-    #     if picking.fiscal_position and \
-    #             picking.fiscal_position.inv_copy_note and \
-    #             picking.fiscal_position.note:
-    #         fp_comment.append(picking.fiscal_position.note)
-    #
-    #     for move in picking.move_lines:
-    #         if move.sale_line_id:
-    #             line = move.sale_line_id
-    #             if line.fiscal_position and \
-    #                     line.fiscal_position.inv_copy_note and \
-    #                     line.fiscal_position.note:
-    #                 if line.fiscal_position.id not in fp_ids:
-    #                     fp_comment.append(line.fiscal_position.note)
-    #                     fp_ids.append(line.fiscal_position.id)
-    #
-    #         if move.product_id.ncm_id:
-    #             fc = move.product_id.ncm_id
-    #             if fc.inv_copy_note and fc.note:
-    #                 if fc.id not in fc_ids:
-    #                     fc_comment.append(fc.note)
-    #                     fc_ids.append(fc.id)
-    #
-    #     result['comment'] = " - ".join(fp_comment + fc_comment)
-    #     result['fiscal_category_id'] = picking.fiscal_category_id and \
-    #         picking.fiscal_category_id.id
-    #     result['fiscal_position'] = picking.fiscal_position and \
-    #         picking.fiscal_position.id
-    #     result['ind_final'] = picking.fiscal_position.ind_final
-    #     return result
-
     def _get_invoice_vals(self, cr, uid, key, inv_type, journal_id, move,
                           context=None):
         """
