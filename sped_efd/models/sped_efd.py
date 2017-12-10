3aa22a596527cd371479b13e5a6ab39dab0a39ea
atilla <atillaa4@hotmail.com>
2017-12-10 10:37:32 -0200

    [WIP] gerar arquivo EFD correto

--- sped_efd/models/sped_efd.py
+++ sped_efd/models/sped_efd.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 import base64
 
-from sped.efd.icms_ipi import arquivos,blocos,registros
+from sped.efd.icms_ipi import arquivos,registros
 from odoo import fields, models, api
-
-
-# erro no bloco 0990 deve ser 4
-# erro no bloco 9990 deve ser 3
-# erro no bloco 9001 deve ser 0
 
 class SpedEFD(models.Model):
     _name='sped.efd'
@@ -30,58 +25,60 @@ class SpedEFD(models.Model):
         required=True,
     )
 
+    def cria_registro(self, registro):
+        junta = ''
+        for i in range(1, len(registro._valores)):
+            junta = junta + '|' + registro._valores[i]
+        return junta
+
     def envia_efd(self):
-        txt = u"""|0000|010|0|01102016|30102016|KMEE INFORMATICA LTDA|53.939.351/0001-29|333.333.333-33|SP|222.222.222.222|1234567|5999|0123|A|1|
-        |0001|0|
-        |0100|Daniel Sadamo|12334532212|532212|||Rua dos ferroviario|123|Agonia||||||
-        |0990|3|
-        |C001|1|
-        |C990|2|
-        |D001|1|
-        |D990|2|
-        |E001|1|
-        |E990|2|
-        |G001|1|
-        |G990|2|
-        |H001|1|
-        |H990|2|
-        |K001|1|
-        |K990|2|
-        |1001|1|
-        |1990|2|
-        |9001|1|
-        |9990|2|
-        |9999|21|
-        """.replace('\n', '\r\n')
         arq = arquivos.ArquivoDigital()
+        arq.read_registro('|9900|0000|1|')
+        arq.read_registro('|9900|9999|1|')
+        cont_9900 = 2
+        registro_0000 = registros.Registro0000()
+        registro_0000.COD_VER = '010'
+        registro_0000.COD_FIN = '0'
+        registro_0000.DT_INI = '01102016'
+        registro_0000.DT_FIN = '30102016'
+        registro_0000.NOME = 'KMEE INFORMATICA LTDA'
+        registro_0000.CNPJ = '53939351000129'
+        registro_0000.UF = 'SP'
+        registro_0000.IE = '22222222222222'
+        registro_0000.COD_MUN = '1234567'
+        registro_0000.IM = '5999'
+        registro_0000.SUFRAMA = '123456789'
+        registro_0000.IND_PERFIL = 'A'
+        registro_0000.IND_ATIV = '1'
+        arq.read_registro(self.cria_registro(registro_0000))
 
-        arq._registro_abertura.COD_VER = '010'
-        arq._registro_abertura.COD_FIN = '0'
-        arq._registro_abertura.DT_INI = '01102016'
-        arq._registro_abertura.DT_FIN = '30102016'
-        arq._registro_abertura.NOME = 'KMEE INFORMATICA LTDA'
-        arq._registro_abertura.CNPJ = '53939351000129'
-        # arq._registro_abertura.CPF = '333.333.333-33'
-        arq._registro_abertura.UF = 'SP'
-        arq._registro_abertura.IE = '22222222222222'
-        arq._registro_abertura.COD_MUN = '1234567'
-        arq._registro_abertura.IM = '5999'
-        arq._registro_abertura.SUFRAMA = '123456789' # 9 digitos
-        arq._registro_abertura.IND_PERFIL = 'A'
-        arq._registro_abertura.IND_ATIV = '1'
+        registro_0100 = registros.Registro0100()
+        registro_0100.NOME = 'Daniel Sadamo'
+        registro_0100.CPF = '12334532212'
+        registro_0100.CRC = '123456789012345'
+        registro_0100.END = 'Rua dos ferroviario'
+        registro_0100.NUM = '123'
+        registro_0100.COMPL = 'Agonia'
+        arq.read_registro(self.cria_registro(registro_0100))
 
-        contabilista = registros.Registro0100()
-        contabilista.NOME = 'Daniel Sadamo'
-        contabilista.CPF = '12334532212'
-        contabilista.CRC = '123456789012345'
-        contabilista.END = 'Rua dos ferroviario'
-        contabilista.NUM = '123'
-        contabilista.COMPL = 'Agonia'
+        for bloco in arq._blocos.items():
+                for registros_bloco in bloco[1].registros:
+                    if not registros_bloco._valores[1] == '9900':
+                        registro_9900 = registros.Registro9900()
+                        registro_9900.REG_BLC = registros_bloco._valores[1]
+                        registro_9900.QTD_REG_BLC = '1'
+                        arq._blocos['9'].add(registro_9900)
+                    else:
+                        cont_9900 = cont_9900 + 1
+
+        registro_9900 = registros.Registro9900()
+        registro_9900.REG_BLC = '9900'
+        registro_9900.QTD_REG_BLC = str(cont_9900+1)
+        arq.read_registro(self.cria_registro(registro_9900))
+
         arquivo = self.env['ir.attachment']
 
-        arq._blocos['0'].add(contabilista)
-        fechamento = registros.Registro0990()
-        fechamento.QTD_LIN_0 = 4
+
         dados = {
             'name': 'teste.txt',
             'datas_fname': 'teste.txt',
