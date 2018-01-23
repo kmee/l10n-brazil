4c4e3fe0df3c9605cf28fbe295919902a63c6e62
Hendrix Costa <hendrixcosta@gmail.com>
2018-01-23 16:19:23 -0200

    [FIX] Refatoração da view e nomes

--- sped_nfe/wizards/wizard_nfe_motivo_cancelamento.py
+++ sped_nfe/wizards/wizard_nfe_motivo_cancelamento.py
@@ -4,32 +4,30 @@ from odoo import api, fields, models, _
 from odoo.exceptions import Warning as UserError
 
 
-class FinanLancamento(models.TransientModel):
+class NfeCancelamentoWizard(models.TransientModel):
     _name = b'nfe.cancelamento.wizard'
-    _inherit = 'sped.documento'
 
-    motivo_cancelamento = fields.Char(
+    motivo_cancelamento = fields.Text(
         string='Justificativa para Cancelamento de NF-e',
         required=True,
     )
 
     @api.multi
-    def motivo_cancelamento_action(self):
+    def action_motivo_cancelamento(self):
+        """
+        
+        :return: 
+        """
+
         self.ensure_one()
 
-        vals = {
-            'justificativa':self.motivo_cancelamento,
-        }
+        if len(self.motivo_cancelamento) < 10:
+            raise UserError("A justificativa deve ter mais de 10 caracteres.")
 
-        nfe = self.env['sped.documento'].browse(
-                self.env.context['active_id'])
+        nfe = self.env['sped.documento'].browse(self.env.context['active_id'])
 
-        if len(self.motivo_cancelamento) > 10:
-            nfe.justificativa = self.motivo_cancelamento
+        nfe.justificativa = self.motivo_cancelamento
 
-            # nfe.cancela_nfe()
-        else:
-            raise UserError("A justificativa deve ter mais de 10"
-                              " caracteres.")
+        nfe.cancela_nfe()
 
-        return nfe
+        return {'type': 'ir.actions.act_window_close'}
