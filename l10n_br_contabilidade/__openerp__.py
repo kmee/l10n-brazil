6cec251fc5b86ddea6ac498cd3a96522a174f864
Daniel Sadamo Hirayama <daniel.sadamo@kmee.com.br>
2019-01-04 20:51:18 -0200

    [ADD]MIS Builder usability features

--- l10n_br_contabilidade/__openerp__.py
+++ l10n_br_contabilidade/__openerp__.py
@@ -40,6 +40,9 @@
         'views/account_historico_padrao.xml',
         # 'views/account_mapeamento.xml', TODO: Refatorar usando constas de consolidação
         'views/account_saldo.xml',
+        'views/mis_report.xml',
+        # 'views/mis_report_instance.xml',
+        'views/mis_report_kpi.xml',
     ],
     'demo': [
     ],
--- l10n_br_contabilidade/models/__init__.py
+++ l10n_br_contabilidade/models/__init__.py
@@ -19,3 +19,4 @@ from . import account_mapeamento
 from . import account_period
 from . import account_saldo
 from . import account_divisao_resultado
+from . import mis_report_kpi
