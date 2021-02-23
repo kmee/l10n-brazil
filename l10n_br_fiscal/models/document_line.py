6a94760b7a36c38f1950fe580b2a7504ca602896
Gabriel Cardoso de Faria <gacarfaria@gmail.com>
2021-02-23 16:41:00 -0300

    [ADD] Recompute document taxes

--- l10n_br_fiscal/models/document_line.py
+++ l10n_br_fiscal/models/document_line.py
@@ -255,3 +255,14 @@ class DocumentLine(models.Model):
         compute='_compute_amount_total_cost',
         default=0.00,
     )
+
+    def recompute_taxes(self):
+        for line in self:
+            price_unit = line.price_unit
+            line._onchange_product_id_fiscal()
+            line._onchange_ncm_id()
+            line._onchange_fiscal_operation_id()
+            line._onchange_fiscal_operation_line_id()
+            line.price_unit = price_unit
+            line._onchange_commercial_quantity()
+            line._onchange_fiscal_taxes()
