32737b1dbc0b2da934e8cb7880c33f3fc75d6959
Luis Malta <luis.malta@kmee.com.br>
2021-03-16 15:57:06 -0300

    [REF] Test NFe Serialize base framework

--- l10n_br_nfe/tests/test_nfe_serialize.py
+++ l10n_br_nfe/tests/test_nfe_serialize.py
@@ -25,21 +25,7 @@ class TestNFeExport(TransactionCase):
         super(TestNFeExport, self).setUp()
         hooks.register_hook(self.env, 'l10n_br_nfe',
                             'odoo.addons.l10n_br_nfe_spec.models.v4_00.leiauteNFe')
-        self.nfe = self.env.ref('l10n_br_nfe.demo_nfe_same_state')
-        self.nfe.write(
-            {'document_type_id': self.env.ref('l10n_br_fiscal.document_55').id,
-             'company_id': self.env.ref('l10n_br_base.empresa_lucro_presumido').id,
-             'company_number': 3,
-             'processador_edoc': 'erpbrasil_edoc',
-             })
-        self.nfe.company_id.processador_edoc = 'erpbrasil_edoc'
-        if self.nfe.state != 'em_digitacao':  # 2nd test run
-            self.nfe.action_document_back2draft()
-
-        for line in self.nfe.line_ids:
-            line._onchange_product_id_fiscal()
-            line._onchange_fiscal_operation_id()
-            line._onchange_fiscal_operation_line_id()
+        self.nfe_list = []
 
         self.cert_country = "BR"
         self.cert_issuer_a = "EMISSOR A TESTE"
@@ -60,7 +46,7 @@ class TestNFeExport(TransactionCase):
             valid=True, passwd=self.cert_passwd, issuer=self.cert_issuer_a,
             country=self.cert_country, subject=self.cert_subject_valid)
 
-        cert = self.certificate_model.create(
+        self.cert = self.certificate_model.create(
             {
                 'type': 'nf-e',
                 'subtype': 'a1',
@@ -68,8 +54,6 @@ class TestNFeExport(TransactionCase):
                 'file': self.certificate_valid
             }
         )
-        self.nfe.company_id.certificate_nfe_id = cert
-        self.nfe.company_id.street_number = '3'
 
     def _create_certificate(self, valid=True, passwd=None, issuer=None,
                             country=None, subject=None):
@@ -106,22 +90,42 @@ class TestNFeExport(TransactionCase):
 
         return b64encode(p12.export(passwd))
 
+    def prepare_test_nfe(self, nfe):
+        """
+        Performs actions necessary to prepare an NFe of the demo data to
+        perform the tests
+        """
+        if nfe.state != 'em_digitacao':  # 2nd test run
+            nfe.action_document_back2draft()
+
+        for line in nfe.line_ids:
+            line._onchange_product_id_fiscal()
+            line._onchange_fiscal_operation_id()
+            line._onchange_fiscal_operation_line_id()
+
     def test_serialize_xml(self):
-        xml_path = os.path.join(
-            l10n_br_nfe.__path__[0], 'tests', 'nfe', 'v4_00', 'leiauteNFe',
-            'NFe35200697231608000169550010000000111855451724-nf-e.xml')
-        self.nfe.action_document_confirm()
-        self.nfe.date = datetime.strptime(
-            '2020-06-04T11:58:46', '%Y-%m-%dT%H:%M:%S')
-        self.nfe.date_in_out = datetime.strptime(
-            '2020-06-04T11:58:46', '%Y-%m-%dT%H:%M:%S')
-        self.nfe.nfe40_cNF = '06277716'
-        self.nfe.nfe40_Id = 'NFe35200781583054000129550010000000011062777160'
-        self.nfe.with_context(lang='pt_BR')._document_export()
-        output = os.path.join(config['data_dir'], 'filestore',
-                              self.cr.dbname, self.nfe.file_xml_id.store_fname)
-        _logger.info("XML file saved at %s" % (output,))
-        self.nfe.company_id.country_id.name = 'Brazil'  # clean mess
-        diff = main.diff_files(xml_path, output)
-        _logger.info("Diff with expected XML (if any): %s" % (diff,))
-        assert len(diff) == 0
+
+        for nfe in self.nfe_list:
+            nfe_id = nfe['record_id']
+
+            self.prepare_test_nfe(nfe_id)
+
+            xml_path = os.path.join(
+                l10n_br_nfe.__path__[0], 'tests', 'nfe', 'v4_00', 'leiauteNFe',
+                nfe['xml_file'])
+            nfe_id.action_document_confirm()
+            nfe_id.date = datetime.strptime(
+                '2020-01-01T11:00:00', '%Y-%m-%dT%H:%M:%S')
+            nfe_id.date_in_out = datetime.strptime(
+                '2020-01-01T11:00:00', '%Y-%m-%dT%H:%M:%S')
+            nfe_id.nfe40_cNF = '06277716'
+            nfe_id.nfe40_Id = 'NFeTest'
+            nfe_id.nfe40_nNF = '1'
+            nfe_id.with_context(lang='pt_BR')._document_export()
+            output = os.path.join(config['data_dir'], 'filestore',
+                                  self.cr.dbname,  nfe_id.file_xml_id.store_fname)
+            _logger.info("XML file saved at %s" % (output,))
+            nfe_id.company_id.country_id.name = 'Brazil'  # clean mess
+            diff = main.diff_files(xml_path, output)
+            _logger.info("Diff with expected XML (if any): %s" % (diff,))
+            assert len(diff) == 0
