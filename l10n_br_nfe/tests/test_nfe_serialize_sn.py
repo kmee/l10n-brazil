0ce5f5c7f35b2db7dc0200b1fac55cdfffed1176
Ygor Carvalho <ygcarvalh@gmail.com>
2021-04-01 10:21:36 -0300

    [FIX] Uncommented testing for 'Simples Nacional'

--- l10n_br_nfe/tests/test_nfe_serialize_sn.py
+++ l10n_br_nfe/tests/test_nfe_serialize_sn.py
@@ -11,11 +11,11 @@ class TestNFeExportLC(TestNFeExport):
         company_id.certificate_nfe_id = self.cert
 
         self.nfe_list = [
-            # {
-            #     'record_id':
-            #         self.env.ref(
-            #             'l10n_br_nfe.demo_nfe_national_sale_for_same_state'),
-            #     'xml_file':
-            #         'NFe35210359594315000157550010000000011291881715-nf-e.xml',
-            # },
+            {
+                'record_id':
+                    self.env.ref(
+                        'l10n_br_nfe.demo_nfe_national_sale_for_same_state'),
+                'xml_file':
+                    'NFe35210359594315000157550010000000011291881715-nf-e.xml',
+            },
         ]
