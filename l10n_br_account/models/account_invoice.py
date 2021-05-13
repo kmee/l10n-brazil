80e41daa7a5215ad9b24137df37f01d08e79af51
Luis Felipe Mileo <mileo@kmee.com.br>
2021-05-13 16:53:29 -0300

    [IMP] Refund account

--- l10n_br_account/models/account_invoice.py
+++ l10n_br_account/models/account_invoice.py
@@ -500,7 +500,6 @@ class AccountInvoice(models.Model):
     def refund(self, date_invoice=None, date=None, description=None, journal_id=None):
         new_invoices = super(AccountInvoice, self).refund(
             date_invoice, date, description, journal_id)
-
         force_fiscal_operation_id = False
         if self.env.context.get('force_fiscal_operation_id'):
             force_fiscal_operation_id = self.env['l10n_br_fiscal.operation'].browse(
@@ -520,6 +519,7 @@ class AccountInvoice(models.Model):
             r.fiscal_operation_id = (
                 force_fiscal_operation_id or
                 r.fiscal_operation_id.return_fiscal_operation_id)
+            r._onchange_account_id()
 
             for line in r.invoice_line_ids:
                 if (not force_fiscal_operation_id and
@@ -532,6 +532,7 @@ class AccountInvoice(models.Model):
                     force_fiscal_operation_id or
                     line.fiscal_operation_id.return_fiscal_operation_id)
                 line._onchange_fiscal_operation_id()
+                line._onchange_fiscal_account_id()
 
             refund_invoice_id = my_new_invoices.refund_invoice_id
 
