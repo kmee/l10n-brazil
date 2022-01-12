4a5297e424d6b5b4d73df56996855fd6ac7c350f
Cristiano Rodrigues <cristiano_one@yahoo.com.br>
2022-01-12 18:29:15 -0300

    [FIX] Removed tour wait time of test

--- payment_pagseguro/tests/test_pagseguro.py
+++ payment_pagseguro/tests/test_pagseguro.py
@@ -13,5 +13,4 @@ class PagseguroTest(HttpCase):
             "odoo.__DEBUG__.services['web_tour.tour'].run('shop_buy_pagseguro')",
             "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_buy_pagseguro.ready",
             login="admin",
-            timeout=40 * 60,
         )
