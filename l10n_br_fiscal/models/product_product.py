73104808049074d3d646e2bb92ef22ac60974644
Renan Hiroki Bastos <hirokibastos@gmail.com>
2022-04-19 13:12:42 -0300

    [ADD] Added icms_regulation_id to product.product view

--- l10n_br_fiscal/models/product_product.py
+++ l10n_br_fiscal/models/product_product.py
@@ -1,9 +1,16 @@
 # Copyright (C) 2021  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models
+from odoo import models, fields
 
 
 class ProductProduct(models.Model):
     _name = "product.product"
     _inherit = ["product.product", "l10n_br_fiscal.product.mixin"]
+
+    icms_regulation_id = fields.Many2one(
+        comodel_name="l10n_br_fiscal.icms.regulation",
+        string="Tax Regulation",
+        related="product_tmpl_id.icms_regulation_id",
+        readonly=False,
+    )
