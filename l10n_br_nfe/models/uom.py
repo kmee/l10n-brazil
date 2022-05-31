d732bd887d424a38edc63a8e3b5f737641574889
Renan Hiroki Bastos <hirokibastos@gmail.com>
2022-05-31 11:18:55 -0300

    Readiciona import api a uom

--- l10n_br_nfe/models/uom.py
+++ l10n_br_nfe/models/uom.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Akretion (Raphaël Valyi <raphael.valyi@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, models
 
 
 class Uom(models.Model):
