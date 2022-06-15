d98e2fc28b00e84e855cdbd707d8ed6a4b2057da
Renan Hiroki Bastos <hirokibastos@gmail.com>
2022-06-15 12:09:22 -0300

    [FIX] Small fixes

--- l10n_br_nfe/models/document_line.py
+++ l10n_br_nfe/models/document_line.py
@@ -38,53 +38,27 @@ class NFeLine(spec_models.StackedModel):
     _force_stack_paths = ("det.imposto.",)
     _stack_skip = ("nfe40_det_infNFe_id",)
 
-    # When dynamic stacking is applied, the NFe line has the following structure:
-    DET_TREE = """
-> <det>
-    > <prod>
-        ≡ <DI>
-        ≡ <detExport>
-        ≡ <rastro>
-        - <infProdNFF>
-        - <infProdEmb>
-        - <veicProd>
-        - <med>
-        ≡ <arma>
-        - <comb>
-    > <imposto>
-        > <ICMS>
-            > <ICMSPart>
-            > <ICMSST>
-        > <IPI>
-            > <IPITrib>
-            > <IPINT>
-        > <II>
-        > <ISSQN>
-        > <PIS>
-            > <PISAliq>
-            > <PISQtde>
-            > <PISNT>
-            > <PISOutr>
-        > <PISST>
-        > <COFINS>
-            > <COFINSAliq>
-            > <COFINSQtde>
-            > <COFINSNT>
-            > <COFINSOutr>
-        > <COFINSST>
-        > <ICMSUFDest>
-    - <impostoDevol>"""
+    # The generateDS prod mixin (prod XML tag) cannot be inject in
+    # the product.product object because the tag embeded values from the
+    # fiscal document line. So the mapping is done:
+    # from Odoo -> XML by using related fields/_compute
+    # from XML -> Odoo by overriding the product create method
+    nfe40_cProd = fields.Char(
+        related="product_id.default_code",
+    )
 
-    ##########################
-    # NF-e spec related fields
-    ##########################
+    nfe40_cEAN = fields.Char(
+        related="product_id.barcode",
+    )
 
     ######################################
     # NF-e tag: prod
     # Grupo I. Produtos e Serviços da NF-e
     ######################################
 
-    # nfe40_cProd = fields.Char(related="product_id.default_code")
+    nfe40_uCom = fields.Char(
+        related="uom_id.code",
+    )
 
     nfe40_cEAN = fields.Char(related="product_id.barcode")
 
