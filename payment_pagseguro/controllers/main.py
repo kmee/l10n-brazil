4c6ecb75909a4480b56de9bc1ef8be124033c0fd
Kilian Macedo Melcher <kilian.melcher@gmail.com>
2022-06-15 13:47:10 -0300

    [IMP] payment_pagseguro: Add notification URL route

--- payment_pagseguro/controllers/main.py
+++ payment_pagseguro/controllers/main.py
@@ -1,11 +1,15 @@
 # Copyright 2020 KMEE INFORMATICA LTDA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import logging
+
 import requests
 import werkzeug
 
 from odoo import http
 from odoo.http import request
+
+_logger = logging.getLogger(__name__)
 
 
 class PagseguroController(http.Controller):
@@ -73,3 +77,28 @@ class PagseguroController(http.Controller):
         public_key = res.get("public_key")
 
         return public_key
+
+    @http.route("/notification-url", auth="public", type="json", methods=["POST"])
+    def notification_url(self):
+        """Receives Pagseguro Charge notification.
+        Returns true on success and False on fail.
+        Since this is a sensitive public route no further information is given.
+        """
+        params = request.jsonrequest
+        notification_code = params.get("id")
+        tx = (
+            request.env["payment.transaction"]
+            .sudo()
+            .search([("acquirer_reference", "=", notification_code)])
+        )
+        tx.ensure_one()
+
+        # Sends requests to pagseguro to check charge status instead of trusting
+        # notification payload
+        try:
+            tx.pagseguro_check_transaction()
+        except Exception as e:
+            _logger.error(e)
+            return False
+
+        return True
