0bb16e96a79c58622331ad3a52534c7411d65516
Renan Hiroki Bastos <hirokibastos@gmail.com>
2022-07-06 16:11:17 -0300

    Added product_code to wizard product line and removed it as search tag for products.

--- l10n_br_nfe/models/product_product.py
+++ l10n_br_nfe/models/product_product.py
@@ -14,7 +14,7 @@ from odoo.osv import expression
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
-    _nfe_search_keys = ["default_code", "barcode"]
+    _nfe_search_keys = ["barcode"]
 
     def match_or_create_m2o(self, rec_dict, parent_dict, model=None):
         create_dict = {}
