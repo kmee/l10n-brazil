63ad765ddd70e854a83841d2182ef64b7a68e7ab
Kilian Macedo Melcher <kilian.melcher@gmail.com>
2022-07-21 08:28:28 -0300

    [IMP] payment_pagseguro: add card brand to payment token

--- payment_pagseguro/models/payment_token.py
+++ payment_pagseguro/models/payment_token.py
@@ -7,24 +7,14 @@ from odoo import fields, models
 class PaymentTokenPagSeguro(models.Model):
     _inherit = "payment.token"
 
-    card_holder = fields.Char(
-        string="Holder",
-        required=False,
-    )
+    pagseguro_card_holder_name = fields.Char(string="Pagseguro Card Holder")
 
-    pagseguro_card_token = fields.Char(
-        string="Pagseguro card Token",
-        required=False,
-    )
+    pagseguro_card_brand = fields.Char(string="Pagseguro Card Brand")
 
-    pagseguro_payment_method = fields.Char(
-        string="Pagseguro payment method",
-        required=False,
-    )
+    pagseguro_card_token = fields.Char(string="Pagseguro Card Token")
 
-    pagseguro_installments = fields.Integer(
-        string="Pagseguro number of installments",
-        required=False,
-    )
+    pagseguro_payment_method = fields.Char(string="Pagseguro Payment Method")
 
-    pagseguro_tx_id = fields.Char(string="Pagseguro transaction id", required=False)
+    pagseguro_installments = fields.Integer(string="Pagseguro Number of Installments")
+
+    pagseguro_tx_id = fields.Char(string="Pagseguro PIX Transaction Id")
