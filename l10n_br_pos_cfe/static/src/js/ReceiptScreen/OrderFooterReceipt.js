ae38b779bf138cec89cfc718e210c76ca27f9a9d
Ygor Carvalho <ygcarvalh@gmail.com>
2022-10-03 07:26:23 -0300

    [IMP] l10n_br_pos_cfe: Inserting the barcode in the print
    It was necessary to add three lines of comment to disable ESLint in
    parts of the code by calling the functions belonging to lib that were
    added to the module `l10n_br_pos` and, consequently, it ends up
    generating an error of no definition.

--- l10n_br_pos_cfe/static/src/js/ReceiptScreen/OrderFooterReceipt.js
+++ l10n_br_pos_cfe/static/src/js/ReceiptScreen/OrderFooterReceipt.js
@@ -5,8 +5,34 @@ odoo.define("l10n_br_pos_cfe.OrderFooterReceipt", function (require) {
     const Registries = require("point_of_sale.Registries");
 
     class OrderFooterReceipt extends PosComponent {
+        mounted() {
+            setTimeout(
+                () => this._generateBarcode(this.order.document_key_formatted),
+                0
+            );
+        }
+
         get order() {
             return this.props.order;
+        }
+
+        _generateBarcode(documentKey) {
+            let s = 1;
+            const barcode = document.getElementsByClassName("satBarcode");
+            // eslint-disable-next-line
+            const dm = datamatrix(documentKey, true);
+            if (dm.length === 0) {
+                s = 2;
+                console.error("Message too long for Data Matrix.");
+            } else {
+                s = Math.floor((400 + dm[1].length) / dm[1].length);
+                // eslint-disable-next-line
+                barcode[0].innerHTML = toHtml(
+                    // eslint-disable-next-line
+                    [code128(documentKey)],
+                    [(s / 4) | 0, 50]
+                );
+            }
         }
 
         // Getters //
