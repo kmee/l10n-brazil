601de9927b24aa0a2593ef34512028e70a18ba74
Ygor Carvalho <ygcarvalh@gmail.com>
2023-03-25 08:55:48 -0300

    [IMP] l10n_br_pos: remove unnecessary files and update manifest

--- l10n_br_pos/__manifest__.py
+++ l10n_br_pos/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Ponto de venda adaptado a legislação Brasileira",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "KMEE, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
     "license": "AGPL-3",
@@ -12,47 +12,19 @@
     "maintainers": ["mileo", "lfdivino", "luismalta", "ygcarvalh"],
     "depends": [
         "l10n_br_fiscal",
-        "l10n_br_account",
         "l10n_br_stock",
         "l10n_br_zip",
         "l10n_br_base",
         "point_of_sale",
-        # TODO: Check this files after alpha version
-        #   "queue_job",
-        #   "l10n_br_stock_account",
-        #   'pos_payment_term',
-        #   'pos_order_picking_link',
-        #   'stock_picking_invoice_link',
-        #   "pos_order_show_list",
-        #   "pos_order_return",
     ],
     "data": [
-        # security
         "security/l10n_br_pos_product_fiscal_map.xml",
-        # data
         "data/l10n_br_fiscal_cfop_data.xml",
-        # Views
         "views/l10n_br_pos_product_fiscal_map.xml",
-        "views/pos_config_view.xml",
         "views/pos_order_view.xml",
         "views/product_template_view.xml",
         "views/res_company.xml",
         "views/pos_payment_method_view.xml",
-        # Templates
-        "views/pos_template.xml",
-        # TODO: Check this files after alpha version
-        #   Report
-        #   "views/point_of_sale_report.xml",
-    ],
-    "demo": [
-        "demo/product_template_demo.xml",
-        "demo/pos_payment_method_demo.xml",
-        "demo/pos_config_demo.xml",
-    ],
-    "qweb": [
-        "static/src/xml/Screens/OrderManagementScreen/ControlButtons/CancelOrderButton.xml",
-        "static/src/xml/Screens/OrderManagementScreen/OrderList.xml",
-        "static/src/xml/Screens/OrderManagementScreen/OrderRow.xml",
     ],
     "installable": True,
 }
