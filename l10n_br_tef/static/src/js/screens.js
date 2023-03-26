1a71bbee17b9f29d195481a5285f19061c687322
Luis Felipe Mileo <mileo@kmee.com.br>
2023-03-26 01:17:19 -0300

    [REM] dead code

--- l10n_br_tef/static/src/js/screens.js
+++ l10n_br_tef/static/src/js/screens.js
@@ -1,85 +1,85 @@
-/*
-    L10n_br_tef
-    Copyright (C) 2018 KMEE INFORMATICA LTDA (http://www.kmee.com.br)
-    @author Atilla Graciano da Silva <atilla.silva@kmee.com.br>
-    @author Bianca da Rocha Bartolomei <bianca.bartolomei@kmee.com.br>
-    @author Hugo Uchôas Borges <hugo.borges@kmee.com.br>
-    @author Luis Felipe Mileo  <mileo@kmee.com.br>
-    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-*/
+// /*
+//     L10n_br_tef
+//     Copyright (C) 2018 KMEE INFORMATICA LTDA (http://www.kmee.com.br)
+//     @author Atilla Graciano da Silva <atilla.silva@kmee.com.br>
+//     @author Bianca da Rocha Bartolomei <bianca.bartolomei@kmee.com.br>
+//     @author Hugo Uchôas Borges <hugo.borges@kmee.com.br>
+//     @author Luis Felipe Mileo  <mileo@kmee.com.br>
+//     License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+// */
 
-odoo.define("l10n_br_tef.screens", function (require) {
-    "use strict";
+// odoo.define("l10n_br_tef.screens", function (require) {
+//     "use strict";
 
-    const PaymentScreen = require("point_of_sale.PaymentScreen");
-    const Registries = require("point_of_sale.Registries");
-    const core = require("web.core");
-    const _t = core._t;
+//     const PaymentScreen = require("point_of_sale.PaymentScreen");
+//     const Registries = require("point_of_sale.Registries");
+//     const core = require("web.core");
+//     const _t = core._t;
 
-    const L10nBrTefPaymentScreen = PaymentScreen =>
-        class extends PaymentScreen {
-            render_paymentlines () {
-                /*
-                * Button bind to start a new TEF transaction
-                */
-                this._super.apply(this, arguments);
-                this.$(".paymentlines-container")
-                    .unbind("click")
-                    .on("click", ".tef-payment-terminal-transaction-start", () => {
-                        this.pos.get_order().tef_in_transaction = true;
-                        this.order_changes();
-                        this.pos.tef_client.start_operation("purchase", this);
-                    });
-            }
+//     const L10nBrTefPaymentScreen = PaymentScreen =>
+//         class extends PaymentScreen {
+//             render_paymentlines () {
+//                 /*
+//                 * Button bind to start a new TEF transaction
+//                 */
+//                 this._super.apply(this, arguments);
+//                 this.$(".paymentlines-container")
+//                     .unbind("click")
+//                     .on("click", ".tef-payment-terminal-transaction-start", () => {
+//                         this.pos.get_order().tef_in_transaction = true;
+//                         this.order_changes();
+//                         this.pos.tef_client.start_operation("purchase", this);
+//                     });
+//             }
 
-            order_changes () {
-                /*
-                * Handles the loading icon for ongoing TEF transactions on
-                *   the payment screen
-                */
-                this._super.apply(this, arguments);
-                var order = this.pos.get_order();
-                if (!order) {
-                    return;
-                } else if (order.tef_in_transaction) {
-                    this.$(".tef_in_transaction").removeClass("oe_hidden");
-                } else {
-                    this.$(".tef_in_transaction").addClass("oe_hidden");
-                }
-            }
+//             order_changes () {
+//                 /*
+//                 * Handles the loading icon for ongoing TEF transactions on
+//                 *   the payment screen
+//                 */
+//                 this._super.apply(this, arguments);
+//                 var order = this.pos.get_order();
+//                 if (!order) {
+//                     return;
+//                 } else if (order.tef_in_transaction) {
+//                     this.$(".tef_in_transaction").removeClass("oe_hidden");
+//                 } else {
+//                     this.$(".tef_in_transaction").addClass("oe_hidden");
+//                 }
+//             }
 
-            finalize_validation () {
-                /*
-                * Block payment completion if there are pending payments via TEF
-                */
-                const payment_lines = this.pos.get_order().get_paymentlines();
-                const has_unpaid_lines = payment_lines.reduce((acc, line) => {
-                    if (!line.tef_payment_completed) {
-                        acc = true;
-                    }
-                    return acc;
-                }, false);
+//             finalize_validation () {
+//                 /*
+//                 * Block payment completion if there are pending payments via TEF
+//                 */
+//                 const payment_lines = this.pos.get_order().get_paymentlines();
+//                 const has_unpaid_lines = payment_lines.reduce((acc, line) => {
+//                     if (!line.tef_payment_completed) {
+//                         acc = true;
+//                     }
+//                     return acc;
+//                 }, false);
 
-                if (has_unpaid_lines) {
-                    this.pos.gui.show_popup("alert", {
-                        title: _t("Unpaid TEF transactions"),
-                        body: _t("There are unpaid TEF transactions"),
-                    });
-                } else {
-                    this._super.apply(this, arguments);
-                }
-            }
+//                 if (has_unpaid_lines) {
+//                     this.pos.gui.show_popup("alert", {
+//                         title: _t("Unpaid TEF transactions"),
+//                         body: _t("There are unpaid TEF transactions"),
+//                     });
+//                 } else {
+//                     this._super.apply(this, arguments);
+//                 }
+//             }
 
-            get_selected_paymentline () {
-                const order = this.pos.get_order();
-                let paymentline = null;
-                order.get_paymentlines().forEach(function (payment) {
-                    if (payment.selected) {
-                        paymentline = payment;
-                    }
-                });
-                return paymentline;
-            }
-        };
-        Registries.Component.extend(PaymentScreen, L10nBrTefPaymentScreen);
-});
+//             get_selected_paymentline () {
+//                 const order = this.pos.get_order();
+//                 let paymentline = null;
+//                 order.get_paymentlines().forEach(function (payment) {
+//                     if (payment.selected) {
+//                         paymentline = payment;
+//                     }
+//                 });
+//                 return paymentline;
+//             }
+//         };
+//         Registries.Component.extend(PaymentScreen, L10nBrTefPaymentScreen);
+// });
--- l10n_br_tef/static/src/js/widgets.js
+++ l10n_br_tef/static/src/js/widgets.js
@@ -1,154 +1,154 @@
-/*
-    L10n_br_tef
-    Copyright (C) 2018 KMEE INFORMATICA LTDA (http://www.kmee.com.br)
-    @author Atilla Graciano da Silva <atilla.silva@kmee.com.br>
-    @author Bianca da Rocha Bartolomei <bianca.bartolomei@kmee.com.br>
-    @author Hugo Uchôas Borges <hugo.borges@kmee.com.br>
-    @author Luis Felipe Mileo  <mileo@kmee.com.br>
-    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-*/
+// /*
+//     L10n_br_tef
+//     Copyright (C) 2018 KMEE INFORMATICA LTDA (http://www.kmee.com.br)
+//     @author Atilla Graciano da Silva <atilla.silva@kmee.com.br>
+//     @author Bianca da Rocha Bartolomei <bianca.bartolomei@kmee.com.br>
+//     @author Hugo Uchôas Borges <hugo.borges@kmee.com.br>
+//     @author Luis Felipe Mileo  <mileo@kmee.com.br>
+//     License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+// */
 
-odoo.define("l10n_br_tef.widgets", function (require) {
-    "use strict";
+// odoo.define("l10n_br_tef.widgets", function (require) {
+//     "use strict";
 
-    const Chrome = require("point_of_sale.Chrome");
-    const Registries = require("point_of_sale.Registries");
-    const gui = require("point_of_sale.Gui");
-    // const PopupWidget = require("point_of_sale.popups");
+//     const Chrome = require("point_of_sale.Chrome");
+//     const Registries = require("point_of_sale.Registries");
+//     const gui = require("point_of_sale.Gui");
+//     // const PopupWidget = require("point_of_sale.popups");
 
-    // class TefStatusWidget extends Chrome.StatusWidget.extend {
+//     // class TefStatusWidget extends Chrome.StatusWidget.extend {
 
-    //     set_tef_status (status) {
-    //         if (status.state === "connected") {
-    //             const warning = false;
-    //             const msg = "";
-    //             this.set_status(warning ? "warning" : "connected", msg);
-    //         } else {
-    //             this.set_status(status.state, "");
-    //         }
-    //     }
+//     //     set_tef_status (status) {
+//     //         if (status.state === "connected") {
+//     //             const warning = false;
+//     //             const msg = "";
+//     //             this.set_status(warning ? "warning" : "connected", msg);
+//     //         } else {
+//     //             this.set_status(status.state, "");
+//     //         }
+//     //     }
 
-    //     start () {
-    //         this.set_tef_status(this.pos.get("tef_status"));
-    //         this.pos.bind("change:tef_status", (pos, tef_status) => {
-    //             this.set_status(tef_status.state, tef_status.pending);
-    //         });
+//     //     start () {
+//     //         this.set_tef_status(this.pos.get("tef_status"));
+//     //         this.pos.bind("change:tef_status", (pos, tef_status) => {
+//     //             this.set_status(tef_status.state, tef_status.pending);
+//     //         });
 
-    //         // Forces reconnection to the TEF client
-    //         this.$el.click(() => {
-    //             this.pos.tef_client.connect();
-    //         });
+//     //         // Forces reconnection to the TEF client
+//     //         this.$el.click(() => {
+//     //             this.pos.tef_client.connect();
+//     //         });
 
-    //         // TODO: Lidar com o cancelamento/estorno;
-    //         // let cancel_btn = $('.btn-cancelar-pagamento');
-    //         // cancel_btn.hide();
-    //         //
-    //         // if (this.pos.config.iface_tef) {
-    //         //     cancel_btn.show();
-    //         //     cancel_btn.unbind('click').on("click", function (event) {
-    //         //         self.pos_widget.payment_screen.cancel_payment()
-    //         //     });
-    //         // }
-    //     }
-    // };
+//     //         // TODO: Lidar com o cancelamento/estorno;
+//     //         // let cancel_btn = $('.btn-cancelar-pagamento');
+//     //         // cancel_btn.hide();
+//     //         //
+//     //         // if (this.pos.config.iface_tef) {
+//     //         //     cancel_btn.show();
+//     //         //     cancel_btn.unbind('click').on("click", function (event) {
+//     //         //         self.pos_widget.payment_screen.cancel_payment()
+//     //         //     });
+//     //         // }
+//     //     }
+//     // };
 
-    // TefStatusWidget.template = "TefStatusWidget"
-    // Registries.Component.add(TefStatusWidget);
+//     // TefStatusWidget.template = "TefStatusWidget"
+//     // Registries.Component.add(TefStatusWidget);
 
-    // widgets.splice(_.indexOf(_.pluck(widgets, "name"), "notification"), 0, {
-    //     name: "l10n_br_tef_status_widget",
-    //     widget: TefStatusWidget,
-    //     append: ".pos-rightheader",
-    //     condition: function () {
-    //         return this.pos.config.iface_tef;
-    //     },
-    // });
+//     // widgets.splice(_.indexOf(_.pluck(widgets, "name"), "notification"), 0, {
+//     //     name: "l10n_br_tef_status_widget",
+//     //     widget: TefStatusWidget,
+//     //     append: ".pos-rightheader",
+//     //     condition: function () {
+//     //         return this.pos.config.iface_tef;
+//     //     },
+//     // });
 
-    // // FIXME: Adapt the popup to the new pos structure
-    // const CancelamentoCompraPopup = PopupWidget.extend({
-    //     template: "PurchaseCancellationWidget",
+//     // // FIXME: Adapt the popup to the new pos structure
+//     // const CancelamentoCompraPopup = PopupWidget.extend({
+//     //     template: "PurchaseCancellationWidget",
 
-    //     show: function (options) {
-    //         const self = this;
-    //         this._super(options);
+//     //     show: function (options) {
+//     //         const self = this;
+//     //         this._super(options);
 
-    //         $(".btn-report_data").unbind("click");
-    //         // TODO: Check if this bind makes sense. At first the search_handler is only used for product search.
-    //         this.el
-    //             .querySelector(".btn-report_data")
-    //             .addEventListener("click", this.search_handler);
-    //         $(".btn-report_data", this.el).click(function () {
-    //             self.pos_widget.product_screen.proceed_cancellation();
-    //         });
+//     //         $(".btn-report_data").unbind("click");
+//     //         // TODO: Check if this bind makes sense. At first the search_handler is only used for product search.
+//     //         this.el
+//     //             .querySelector(".btn-report_data")
+//     //             .addEventListener("click", this.search_handler);
+//     //         $(".btn-report_data", this.el).click(function () {
+//     //             self.pos_widget.product_screen.proceed_cancellation();
+//     //         });
 
-    //         $(".btn-cancel-operation").unbind("click");
-    //         // TODO: Check if this bind makes sense. At first the search_handler is only used for product search.
-    //         this.el
-    //             .querySelector(".btn-cancel-operation")
-    //             .addEventListener("click", this.search_handler);
-    //         $(".btn-cancel-operation", this.el).click(function () {
-    //             self.pos_widget.product_screen.abort();
-    //         });
-    //     },
-    // });
+//     //         $(".btn-cancel-operation").unbind("click");
+//     //         // TODO: Check if this bind makes sense. At first the search_handler is only used for product search.
+//     //         this.el
+//     //             .querySelector(".btn-cancel-operation")
+//     //             .addEventListener("click", this.search_handler);
+//     //         $(".btn-cancel-operation", this.el).click(function () {
+//     //             self.pos_widget.product_screen.abort();
+//     //         });
+//     //     },
+//     // });
 
-    // // FIXME: Adapt the popup to the new pos structure
-    // const ConfirmaCancelamentoCompraPopup = PopupWidget.extend({
-    //     template: "PurchaseCancellationConfirmWidget",
+//     // // FIXME: Adapt the popup to the new pos structure
+//     // const ConfirmaCancelamentoCompraPopup = PopupWidget.extend({
+//     //     template: "PurchaseCancellationConfirmWidget",
 
-    //     show: function (options) {
-    //         const self = this;
-    //         this._super();
+//     //     show: function (options) {
+//     //         const self = this;
+//     //         this._super();
 
-    //         this.message = options.message || "";
-    //         this.comment = options.comment || "";
-    //         this.renderElement();
+//     //         this.message = options.message || "";
+//     //         this.comment = options.comment || "";
+//     //         this.renderElement();
 
-    //         $(".btn-confirm-cancellation").unbind("click");
-    //         // TODO: Check if this bind makes sense. At first the search_handler is only used for product search.
-    //         this.el
-    //             .querySelector(".btn-confirm-cancellation")
-    //             .addEventListener("click", this.search_handler);
-    //         $(".btn-confirm-cancellation", this.el).click(function () {
-    //             self.pos_widget.product_screen.confirm_proceed_cancellation(true);
-    //         });
+//     //         $(".btn-confirm-cancellation").unbind("click");
+//     //         // TODO: Check if this bind makes sense. At first the search_handler is only used for product search.
+//     //         this.el
+//     //             .querySelector(".btn-confirm-cancellation")
+//     //             .addEventListener("click", this.search_handler);
+//     //         $(".btn-confirm-cancellation", this.el).click(function () {
+//     //             self.pos_widget.product_screen.confirm_proceed_cancellation(true);
+//     //         });
 
-    //         $(".btn-cancel-cancellation").unbind("click");
-    //         // TODO: Check if this bind makes sense. At first the search_handler is only used for product search.
-    //         this.el
-    //             .querySelector(".btn-cancel-cancellation")
-    //             .addEventListener("click", this.search_handler);
-    //         $(".btn-cancel-cancellation", this.el).click(function () {
-    //             self.pos_widget.product_screen.confirm_proceed_cancellation(false);
-    //         });
-    //     },
-    // });
+//     //         $(".btn-cancel-cancellation").unbind("click");
+//     //         // TODO: Check if this bind makes sense. At first the search_handler is only used for product search.
+//     //         this.el
+//     //             .querySelector(".btn-cancel-cancellation")
+//     //             .addEventListener("click", this.search_handler);
+//     //         $(".btn-cancel-cancellation", this.el).click(function () {
+//     //             self.pos_widget.product_screen.confirm_proceed_cancellation(false);
+//     //         });
+//     //     },
+//     // });
 
-    // const StatusPagementoPopUp = PopupWidget.extend({
-    //     template: "PaymentStatusWidget",
+//     // const StatusPagementoPopUp = PopupWidget.extend({
+//     //     template: "PaymentStatusWidget",
 
-    //     show: function (options) {
-    //         this._super();
-    //         this.message = options.title || "";
-    //         this.comment = options.body || "";
-    //         this.renderElement();
-    //     },
-    // });
+//     //     show: function (options) {
+//     //         this._super();
+//     //         this.message = options.title || "";
+//     //         this.comment = options.body || "";
+//     //         this.renderElement();
+//     //     },
+//     // });
 
-    // gui.define_popup({
-    //     name: "CancelamentoCompraPopup",
-    //     widget: CancelamentoCompraPopup,
-    // });
-    // gui.define_popup({
-    //     name: "ConfirmaCancelamentoCompraPopup",
-    //     widget: ConfirmaCancelamentoCompraPopup,
-    // });
-    // gui.define_popup({name: "StatusPagementoPopUp", widget: StatusPagementoPopUp});
+//     // gui.define_popup({
+//     //     name: "CancelamentoCompraPopup",
+//     //     widget: CancelamentoCompraPopup,
+//     // });
+//     // gui.define_popup({
+//     //     name: "ConfirmaCancelamentoCompraPopup",
+//     //     widget: ConfirmaCancelamentoCompraPopup,
+//     // });
+//     // gui.define_popup({name: "StatusPagementoPopUp", widget: StatusPagementoPopUp});
 
-    return {
-        // TefStatusWidget: TefStatusWidget,
-        // CancelamentoCompraPopup: CancelamentoCompraPopup,
-        // ConfirmaCancelamentoCompraPopup: ConfirmaCancelamentoCompraPopup,
-        // StatusPagementoPopUp: StatusPagementoPopUp,
-    };
-});
+//     return {
+//         // TefStatusWidget: TefStatusWidget,
+//         // CancelamentoCompraPopup: CancelamentoCompraPopup,
+//         // ConfirmaCancelamentoCompraPopup: ConfirmaCancelamentoCompraPopup,
+//         // StatusPagementoPopUp: StatusPagementoPopUp,
+//     };
+// });
