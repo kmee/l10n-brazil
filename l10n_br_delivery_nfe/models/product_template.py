456b4ca9bce80533eb27262bfc6560fe85ed7bb0
Diego Paradeda <diego.paradeda@kmee.com.br>
2024-05-28 14:27:15 -0300

    [ADD] marca and especie fields and compute

---
+++ l10n_br_delivery_nfe/models/product_template.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2024 Diego Paradeda - KMEE
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    product_nfe40_esp = fields.Char(
+        string="Espécie dos volumes transportados",
+        related="product_variant_ids.product_nfe40_esp",
+        readonly=False,
+    )
+
+    product_nfe40_marca = fields.Char(
+        string="Marca dos volumes transportados",
+        related="product_variant_ids.product_nfe40_marca",
+        readonly=False,
+    )
+
+    # Manter compatibilidade com módulo: product_net_weight
+    net_weight = fields.Float(
+        string="Net Weight",
+        related="product_variant_ids.net_weight",
+        help="Net Weight of the product, container excluded.",
+        readonly=False,
+    )
