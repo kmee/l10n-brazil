d1ac462671e8f7bc715afd60691bb11676d4bc24
Diego Paradeda <diego.paradeda@kmee.com.br>
2024-05-31 14:25:07 -0300

    [ADD] multi-transfer invoicing support

--- l10n_br_delivery_nfe/wizards/stock_invoice_onshipping.py
+++ l10n_br_delivery_nfe/wizards/stock_invoice_onshipping.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2024 Diego Paradeda - KMEE
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class StockInvoiceOnshipping(models.TransientModel):
@@ -17,13 +18,14 @@ class StockInvoiceOnshipping(models.TransientModel):
         invoice, values = super()._build_invoice_values_from_pickings(pickings)
 
         # Volumes
+        vol_ids = self.vol_ids.filtered(lambda v: v.picking_id == pickings)
         values["nfe40_vol"] = [
             (
                 0,
                 0,
                 self._convert_volumes_to_write(vol_id),
             )
-            for vol_id in self.vol_ids
+            for vol_id in vol_ids
         ]
 
         return invoice, values
@@ -39,75 +41,82 @@ class StockInvoiceOnshipping(models.TransientModel):
         }
 
     def _get_picking(self):
-        active_ids = self.env.context.get("active_ids", [])
-        if active_ids:
-            active_ids = active_ids[0]
-        pick_obj = self.env["stock.picking"]
-        return pick_obj.browse(active_ids)
+        picking_ids = self._load_pickings()
+        if not picking_ids:
+            raise UserError(_("No picking 2binvoiced!"))
+        return picking_ids
 
     def _get_volume_data_package_level(self):
         """Generate a single volume for packages"""
-        picking_id = self._get_picking()
+        picking_ids = self._get_picking()
 
         vols_data = []
-        if picking_id.package_ids:
-            for package_level_id in picking_id.package_level_ids:
-                manual_weight = package_level_id.package_id.shipping_weight
-                vol_data = {
-                    "nfe40_qVol": 1,
-                    "nfe40_esp": "",
-                    "nfe40_marca": "",
-                    "nfe40_pesoL": 0,
-                    "nfe40_pesoB": (manual_weight if manual_weight else 0),
-                }
+        for picking_id in picking_ids:
+            if picking_id.package_ids:
+                for package_level_id in picking_id.package_level_ids:
+                    manual_weight = package_level_id.package_id.shipping_weight
+                    vol_data = {
+                        "nfe40_qVol": 1,
+                        "nfe40_esp": "",
+                        "nfe40_marca": "",
+                        "nfe40_pesoL": 0,
+                        "nfe40_pesoB": (manual_weight if manual_weight else 0),
+                        "picking_id": picking_id.id,
+                    }
 
-                for line in package_level_id.move_line_ids:
-                    vol_data["nfe40_esp"] = (
-                        vol_data["nfe40_esp"] or line.product_id.product_nfe40_esp
-                    )
-                    vol_data["nfe40_marca"] = (
-                        vol_data["nfe40_marca"] or line.product_id.product_nfe40_marca
-                    )
-                    pesoL = line.qty_done * line.product_id.net_weight
-                    pesoB = line.qty_done * line.product_id.weight
-                    vol_data["nfe40_pesoL"] += pesoL
-                    vol_data["nfe40_pesoB"] += 0 if manual_weight else pesoB
-                vols_data.append(vol_data)
+                    for line in package_level_id.move_line_ids:
+                        vol_data["nfe40_esp"] = (
+                            vol_data["nfe40_esp"] or line.product_id.product_nfe40_esp
+                        )
+                        vol_data["nfe40_marca"] = (
+                            vol_data["nfe40_marca"]
+                            or line.product_id.product_nfe40_marca
+                        )
+                        pesoL = line.qty_done * line.product_id.net_weight
+                        pesoB = line.qty_done * line.product_id.weight
+                        vol_data["nfe40_pesoL"] += pesoL
+                        vol_data["nfe40_pesoB"] += 0 if manual_weight else pesoB
+                    vols_data.append(vol_data)
 
         return vols_data
 
     def _get_volume_data_wo_package(self):
         """Generate a single volume for lines without package"""
-        picking_id = self._get_picking()
-        # Filter out move lines with in a package
-        if not picking_id.move_line_ids_without_package.filtered(
-            lambda ml: not ml.package_level_id
-        ):
-            return []
+        picking_ids = self._get_picking()
 
-        vols_data = [
-            {
+        vols_data = []
+        for picking_id in picking_ids:
+            # Filter out move lines with in a package
+            if not picking_id.move_line_ids_without_package.filtered(
+                lambda ml: not ml.package_level_id
+            ):
+                continue
+
+            new_vol = {
                 "nfe40_qVol": 0,
                 "nfe40_esp": "",
                 "nfe40_marca": "",
                 "nfe40_pesoL": 0,
                 "nfe40_pesoB": 0,
+                "picking_id": picking_id.id,
             }
-        ]
-        for line in picking_id.move_line_ids_without_package.filtered(
-            lambda ml: not ml.package_level_id
-        ):
-            vols_data[0]["nfe40_qVol"] += line.qty_done
-            vols_data[0]["nfe40_esp"] = (
-                vols_data[0]["nfe40_esp"] or line.product_id.product_nfe40_esp
-            )
-            vols_data[0]["nfe40_marca"] = (
-                vols_data[0]["nfe40_marca"] or line.product_id.product_nfe40_marca
-            )
-            pesoL = line.qty_done * line.product_id.net_weight
-            pesoB = line.qty_done * line.product_id.weight
-            vols_data[0]["nfe40_pesoL"] += pesoL
-            vols_data[0]["nfe40_pesoB"] += pesoB
+
+            for line in picking_id.move_line_ids_without_package.filtered(
+                lambda ml: not ml.package_level_id and not ml.result_package_id
+            ):
+                new_vol["nfe40_qVol"] += line.qty_done
+                new_vol["nfe40_esp"] = (
+                    new_vol["nfe40_esp"] or line.product_id.product_nfe40_esp
+                )
+                new_vol["nfe40_marca"] = (
+                    new_vol["nfe40_marca"] or line.product_id.product_nfe40_marca
+                )
+                pesoL = line.qty_done * line.product_id.net_weight
+                pesoB = line.qty_done * line.product_id.weight
+                new_vol["nfe40_pesoL"] += pesoL
+                new_vol["nfe40_pesoB"] += pesoB
+
+            vols_data.append(new_vol)
 
         return vols_data
 
@@ -129,13 +138,3 @@ class StockInvoiceOnshipping(models.TransientModel):
         vol_ids = [(0, 0, vol) for vol in self.prepare_vols_data()]
         result["vol_ids"] = vol_ids
         return result
-
-
-class StockInvoiceOnshippingVol(models.TransientModel):
-    _name = "stock.invoice.onshipping.vol"
-    _inherit = "nfe.40.vol"
-
-    invoice_wizard_id = fields.Many2one(
-        string="stock.invoice.onshipping",
-        comodel_name="stock.invoice.onshipping",
-    )
