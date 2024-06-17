ec210d09ffe7de9c1b1983a28a775876f20e6b2a
Diego Paradeda <diego.paradeda@kmee.com.br>
2024-06-17 15:33:48 -0300

    [FIX] prod/templ nfe40_esp - use compute/inverse

--- l10n_br_delivery_nfe/models/product_template.py
+++ l10n_br_delivery_nfe/models/product_template.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2024 Diego Paradeda - KMEE
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
@@ -9,6 +9,24 @@ class ProductTemplate(models.Model):
 
     product_nfe40_esp = fields.Char(
         string="Type of transported volumes",
-        related="product_variant_ids.product_nfe40_esp",
-        readonly=False,
+        compute="_compute_nfe40_esp",
+        inverse="_inverse_nfe40_esp",
+        store=True,
     )
+
+    @api.depends("product_variant_ids", "product_variant_ids.product_nfe40_esp")
+    def _compute_nfe40_esp(self):
+        unique_variants = self.filtered(
+            lambda template: len(template.product_variant_ids) == 1
+        )
+        for template in unique_variants:
+            template.product_nfe40_esp = template.product_variant_ids.product_nfe40_esp
+        for template in self - unique_variants:
+            template.product_nfe40_esp = ""
+
+    def _inverse_nfe40_esp(self):
+        for template in self:
+            if len(template.product_variant_ids) == 1:
+                template.product_variant_ids.product_nfe40_esp = (
+                    template.product_nfe40_esp
+                )
