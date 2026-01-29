8c98161d9a5b4d5fdac8aa424e2ed5467d59c7d4
Diego Paradeda <diego.paradeda@kmee.com.br>
2026-01-29 16:43:57 -0300

    [WIP] migration

--- l10n_br_coa_simple/hooks.py
+++ l10n_br_coa_simple/hooks.py
@@ -7,15 +7,13 @@ from odoo import SUPERUSER_ID, api, tools
 def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
 
-    # Load COA for demo Company
+    # Load COA for SN Company
     company_sn = env.ref(
         "l10n_br_base.empresa_simples_nacional", raise_if_not_found=False
     )
     if company_sn:
-        coa_simple_tmpl = env.ref(
-            "l10n_br_coa_simple.l10n_br_coa_simple_chart_template"
-        )
-        coa_simple_tmpl.try_loading(company=company_sn)
+        chart_template = env["account.chart.template"]
+        chart_template.try_loading("br_oca_simple", company_sn, install_demo=True)
         tools.convert_file(
             cr,
             "l10n_br_coa_simple",
