413573df5e01e55189c5016cd3fc34bdcba7befe
Ygor Carvalho <ygcarvalh@gmail.com>
2026-04-08 14:49:59 -0300

    [MIG] l10n_br_account_service_type_by_partner: Migration to 18.0

---
+++ l10n_br_account_service_type_by_partner/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Brazilian Account Service Type by Partner",
+    "summary": "Brazilian Account Service Type by Partner",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-brazil",
+    "depends": ["l10n_br_account"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/service_type_view.xml",
+        "views/res_partner_view.xml",
+    ],
+}
