7f1d625782dddcd9b230bb209dec1ea9c4f66a25
Ygor Carvalho <ygcarvalh@gmail.com>
2026-04-13 15:57:03 -0300

    fix: remove test: true from tour registration (invalid in v18)

--- l10n_br_portal/static/tests/tours/l10n_br_portal_tour.esm.js
+++ l10n_br_portal/static/tests/tours/l10n_br_portal_tour.esm.js
@@ -4,7 +4,6 @@ import {registry} from "@web/core/registry";
 
 registry.category("web_tour.tours").add("l10n_br_portal_tour", {
     url: "/my/account",
-    test: true,
     steps: () => [
         {
             content: "Complete name",
