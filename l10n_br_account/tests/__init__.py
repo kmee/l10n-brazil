a87f53c5b345c87ec50a0c5d43072a7b04265091
Ygor Carvalho <ygor.carvalho@kmee.com.br>
2026-08-31 14:05:40 -0300

    [ADD] l10n_br_account: import declaration wizard

--- l10n_br_account/tests/__init__.py
+++ l10n_br_account/tests/__init__.py
@@ -15,3 +15,4 @@ from . import test_import_fiscal_document
 from . import test_move_tax_totals_view
 from . import test_imported_tax_override
 from . import test_imported_due_date
+from . import test_import_declaration_wizard
